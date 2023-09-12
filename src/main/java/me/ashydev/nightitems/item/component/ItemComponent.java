e12a206cbf050f4f7f0e555b41f210ba65fe822c
BuildTools <unconfigured@null.spigotmc.org>
2023-09-12 17:32:41 +0100

    Fix

--- src/main/java/me/ashydev/nightitems/item/component/ItemComponent.java
+++ src/main/java/me/ashydev/nightitems/item/component/ItemComponent.java
@@ -9,7 +9,11 @@ import java.util.List;
 
 public interface ItemComponent extends Nameable, SerializableComponent{
 
+    @Override
+    default NBTCompound serialize(NBTCompound input) { return input; }
 
+    @Override
+    default void deserialize(NBTCompound input) { }
     @Override
     default String getId() {
         return getName().toLowerCase().replace(" ", "-");
